fcdba2220e15078117c5f257b7c543b15aa53ab5
Dipankar Adhikary <70569130+adhikary-dipankar@users.noreply.github.com>
2023-09-05 15:26:31 +0530

    Update Exception.cs

--- Exception.cs
+++ Exception.cs
@@ -1,3 +1,122 @@
+
+
+
+
+using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<Employee> employees = new List<Employee>();
+        
+        while (true)
+        {
+            Console.WriteLine("Menu:");
+            Console.WriteLine("1. Enter new Employee");
+            Console.WriteLine("2. List Employees");
+            Console.WriteLine("3. Exit");
+            Console.Write("Enter your choice: ");
+            
+            int choice = int.Parse(Console.ReadLine());
+            
+            switch (choice)
+            {
+                case 1:
+                    try
+                    {
+                        Console.Write("Enter Employee name: ");
+                        string name = Console.ReadLine();
+                        
+                        if (employees.Exists(e => e.Name == name))
+                        {
+                            throw new DuplicateNameException();
+                        }
+                        
+                        Console.Write("Enter Employee age: ");
+                        int age = int.Parse(Console.ReadLine());
+                        
+                        if (age < 18)
+                        {
+                            throw new AgeException("Children not allowed as an Employee");
+                        }
+                        else if (age > 60)
+                        {
+                            throw new AgeException("Seniors not allowed as an Employee");
+                        }
+                        else if (age > 100)
+                        {
+                            throw new AgeException("Purvaj not allowed as an Employee");
+                        }
+                        
+                        employees.Add(new Employee(name, age));
+                        Console.WriteLine("Employee added successfully.");
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine($"Error: {ex.Message}");
+                    }
+                    break;
+                    
+                case 2:
+                    Console.WriteLine("List of Employees:");
+                    foreach (var employee in employees)
+                    {
+                        Console.WriteLine($"Name: {employee.Name}, Age: {employee.Age}");
+                    }
+                    break;
+                    
+                case 3:
+                    Console.WriteLine("Exiting program.");
+                    Environment.Exit(0);
+                    break;
+                    
+                default:
+                    Console.WriteLine("Invalid choice. Please select a valid option.");
+                    break;
+            }
+        }
+    }
+}
+
+class Employee
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+
+    public Employee(string name, int age)
+    {
+        Name = name;
+        Age = age;
+    }
+}
+
+class AgeException : Exception
+{
+    public AgeException(string message) : base(message)
+    {
+    }
+}
+
+class DuplicateNameException : Exception
+{
+    public DuplicateNameException() : base("Duplicate Name: Not allowed to enter twice (Need double Salary??)")
+    {
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
 using System;
 using System.Collections.Generic;
 
