0bc8d1b5437844bfb5028cab0fd0ddb5d25e5cb3
Dipankar Adhikary <70569130+adhikary-dipankar@users.noreply.github.com>
2023-09-11 11:15:41 +0530

    Update collection.cs

--- collection.cs
+++ collection.cs
@@ -33,3 +33,26 @@
 //        }
 //    }
 //}
+
+
+Ask the user 4 (true/false) questions and compare the answers that user enters with the actual answers
+
+to calculate the marks. Use BitArray to store user's and actual answers.
+
+Q1: Collection namespace has ICollection
+
+interface(T/F):
+
+Q2: An IList in C# represents the class List in other
+
+Languages(T/F): Q3: An Array and any collection is interchangeable
+
+object Types (T/F): Q4: Collection is a part of BCL in .Net Framework
+
+(T/F):
+
+BitArray actual-new BitArray(4).
+
+actual[0]=true, actual[1] = fa BitArray actual new BitArray(4);
+
+actual[0]= true; actual[1]= false; actual[2] = true;
