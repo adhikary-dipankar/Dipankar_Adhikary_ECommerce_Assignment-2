5343a5a31f6fc2f467e950b7415db5ef01e6d4d5
Dipankar Adhikary <70569130+adhikary-dipankar@users.noreply.github.com>
2023-09-21 12:17:09 +0530

    Update SQL_21st_Sept.sql

--- SQL_21st_Sept.sql
+++ SQL_21st_Sept.sql
@@ -9,3 +9,42 @@ select chkPrime(8); --even
 select chkPrime(13); --oddPrime
 select chkPrime(9); --odd
 select list_price, chkPrime(list_price) as ckPrime from ..products;
+
+
+
+CREATE FUNCTION chkPrime(@n INT)
+RETURNS VARCHAR(20)
+AS
+BEGIN
+    DECLARE @isPrime BIT = 1;
+    
+    IF @n <= 1
+        SET @isPrime = 0;
+    ELSE
+    BEGIN
+        DECLARE @i INT = 2;
+        WHILE @i <= SQRT(@n)
+        BEGIN
+            IF @n % @i = 0
+            BEGIN
+                SET @isPrime = 0;
+                BREAK;
+            END
+            SET @i = @i + 1;
+        END
+    END
+    
+    IF @isPrime = 1
+        RETURN 'oddprime';
+    ELSE IF @n % 2 = 0
+        RETURN 'even';
+    ELSE
+        RETURN 'odd';
+END;
+
+
+-- Example usage
+SELECT chkPrime(8); -- Returns 'even'
+SELECT chkPrime(13); -- Returns 'oddprime'
+SELECT chkPrime(9); -- Returns 'odd'
+SELECT list_price, chkPrime(list_price) AS ckPrime FROM products;
